02a288ca755261ca6b5a47099bf29306d05577ec
Reden John Zarra <redjohnzarra@gmail.com>
2019-07-12 14:21:08 +0800

    Integrated firebase, updated sign in page with styles

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,6 +5,8 @@ import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+
 class SignIn extends Component {
   constructor(props) {
     super(props);
@@ -54,7 +56,12 @@ class SignIn extends Component {
             label="password"
             required
           />
-          <CustomButton>Sign In</CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit">Sign In</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign In with Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: 'AIzaSyCI61JDaRLs_jAtDM3C7jwWmPqstE7gCUI',
+  authDomain: 'shop-db-e5f0c.firebaseapp.com',
+  databaseURL: 'https://shop-db-e5f0c.firebaseio.com',
+  projectId: 'shop-db-e5f0c',
+  storageBucket: '',
+  messagingSenderId: '534414793746',
+  appId: '1:534414793746:web:5ea6bdf072619ce6',
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
